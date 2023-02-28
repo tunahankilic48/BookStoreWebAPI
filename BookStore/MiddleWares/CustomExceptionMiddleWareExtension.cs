0d27dc5c3a8165dbecb19ab7e22242f84a7c415e
Tunahan Kilic <tunahankilic942@gmail.com>
2023-02-28 13:57:49 +0300

    Exception middle ware layer was added

---
+++ BookStore/MiddleWares/CustomExceptionMiddleWareExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.MiddleWares
+{
+    public static class CustomExceptionMiddleWareExtension
+    {
+        public static IApplicationBuilder UsecustomExceptionMiddle(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionMiddleWare>();
+
+        }
+    }
+}
